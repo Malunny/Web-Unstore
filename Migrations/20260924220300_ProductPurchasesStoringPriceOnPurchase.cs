03143e8bc81e0ad09c2ee114dc97d14fa0af39dd
Malunny <jhonnybravocf@gmail.com>
2026-09-24 19:05:07 -0300

    feat: ProductPurchase Migrations

---
+++ Migrations/20260924220300_ProductPurchasesStoringPriceOnPurchase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unstore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductPurchasesStoringPriceOnPurchase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PriceOnPurchase",
+                table: "ProductPurchases",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceOnPurchase",
+                table: "ProductPurchases");
+        }
+    }
+}
